d5d42b938ba98316028d4864271d2dd7ca6ef418
plato81 <39441332+plato81@users.noreply.github.com>
2018-05-19 10:43:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
-// Load up the discord.js library
 const Discord = require("discord.js");
 
-// This is your client. Some people call it `bot`, some people call it `self`, 
-// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
-// this is what we're refering to. Your client.
 const client = new Discord.Client();
 
-// Here we load the config.json file that contains our token and our prefix values. 
 //const config = require("./config.json");
-// config.token contains the bot's token
-// config.prefix contains the message prefix.
+
 
 client.on("ready", () => {
-  // This event will run if the bot starts, and logs in, successfully.
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
